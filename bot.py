4c98e38cf1d80c320bc2740ec97310f2694641bd
Sajal Duppatla <sajal.duppatla@gmail.com>
2023-09-28 20:06:51 -0400

    Added support to mute user upon joining voice channel

--- bot.py
+++ bot.py
@@ -12,7 +12,11 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+intents.voice_states = True
 bot = commands.Bot(intents=intents, command_prefix='!')
+
+# For now just have one user that we kick (_lajas's id)
+user_id = 513915310496153600
 
 @bot.event
 async def on_ready():
@@ -22,5 +26,16 @@ async def on_ready():
     
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.id == user_id and after.channel and before.channel != after.channel:
+        if True:
+            print(f"Muting user {member.name} in channel {after.channel.name}")
+            await member.edit(mute=True)
+        else:
+            print("kicking user from channel!")
+            # TODO: kick user :)
+        
 
 bot.run(TOKEN)
